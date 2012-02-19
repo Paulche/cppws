c8f3831e38ced3fc0c9265dfbd761db5f5080436
Paul Chechetin <paulche@yandex.ru>
2012-02-19 11:35:48 +0400

    Update autotools boilerplate

--- autotools/hw/src/main.cpp
+++ autotools/hw/src/main.cpp
@@ -1,34 +1,6 @@
-/* 
- * File:   main.cpp
- * Author: Paul Chechetin
- *
- * Created on February 10, 2012, 11:53 PM
- */
-
-#include <cstdlib>
 #include <iostream>
 
-
-#include "Finder.h"
-
-
-extern void test();
-
-int main(int argc, char** argv) 
+int main ( int argc, char ** argv )
 {
-    
-    PrimeFinder::Finder finder = PrimeFinder::Finder();
-    
-    unsigned int num = atoi(argv[1]);
-    
-    long prime = finder.find(num);
-    
-    std::cout << prime << std::endl;
-        
-    /*
-    test();
-    */
-    
-    return 0;
+    std::cout << "Hello world" << std::endl;
 }
-
